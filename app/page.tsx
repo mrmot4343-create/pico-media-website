de4f4668a822593a6515fde8236eba42b1af641b
mrmot4343-create <mrmot4343@gmail.com>
2026-01-11 17:33:59 +0300

    Create Home component for Pico Media website

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      Pico Media Website is Live 🚀
+    </main>
+  );
+}
